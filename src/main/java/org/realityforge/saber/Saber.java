775574f0327c46b47ea589134da94bf2cd553dde
Peter Donald <peter@realityforge.org>
2019-09-04 09:18:35 +1000

    Initial code

---
+++ src/main/java/org/realityforge/saber/Saber.java
@@ -0,0 +1,12 @@
+package org.realityforge.saber;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Saber
+  implements EntryPoint
+{
+  @Override
+  public void onModuleLoad()
+  {
+  }
+}
