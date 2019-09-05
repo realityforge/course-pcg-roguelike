ff6295d035e763891d3ac23899189877eadfaf70
Peter Donald <peter@realityforge.org>
2019-09-05 19:53:43 +1000

    Define constants for tile types

---
+++ src/main/java/org/realityforge/saber/Tiles.java
@@ -0,0 +1,32 @@
+package org.realityforge.saber;
+
+public final class Tiles
+{
+  public static final int WALL_SINGLE = 0;
+  public static final int WALL_TOP_END = 1;
+  public static final int WALL_SIDE_RIGHT_END = 2;
+  public static final int WALL_BOTTOM_LEFT = 3;
+  public static final int WALL_BOTTOM_END = 4;
+  public static final int WALL_SIDE = 5;
+  public static final int WALL_TOP_LEFT = 6;
+  public static final int WALL_SIDE_LEFT_T = 7;
+  public static final int WALL_SIDE_LEFT_END = 8;
+  public static final int WALL_BOTTOM_RIGHT = 9;
+  public static final int WALL_TOP = 10;
+  public static final int WALL_BOTTOM_T = 11;
+  public static final int WALL_TOP_RIGHT = 12;
+  public static final int WALL_SIDE_RIGHT_T = 13;
+  public static final int WALL_TOP_T = 14;
+  public static final int WALL_INTERSECTION = 15;
+  public static final int WALL_DOOR_LOCKED = 16;
+  public static final int WALL_DOOR_UNLOCKED = 17;
+  public static final int WALL_ENTRANCE = 18;
+  public static final int FLOOR = 19;
+  public static final int FLOOR_ALT = 20;
+  public static final int EMPTY = 21;
+  public static final int COUNT = 22;
+
+  private Tiles()
+  {
+  }
+}
