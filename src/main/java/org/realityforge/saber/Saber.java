ff2084465fa0888ee10e2baecacdca826fb5f658
Peter Donald <peter@realityforge.org>
2019-09-06 17:35:45 +1000

    Reorder tile definitions in order of text filename

--- src/main/java/org/realityforge/saber/Saber.java
+++ src/main/java/org/realityforge/saber/Saber.java
@@ -21,32 +21,27 @@ public class Saber
     _textureManager = new TextureManager( this::startGame );
     _tileTypeManager = new TileTypeManager();
 
+    registerTile( "resources/tiles/spr_tile_door_locked", Tiles.WALL_DOOR_LOCKED );
+    registerTile( "resources/tiles/spr_tile_door_unlocked", Tiles.WALL_DOOR_UNLOCKED, TileType.Flags.SOLID );
     registerTile( "resources/tiles/spr_tile_floor", Tiles.FLOOR, TileType.Flags.SOLID | TileType.Flags.FLOOR );
     registerTile( "resources/tiles/spr_tile_floor_alt", Tiles.FLOOR_ALT, TileType.Flags.SOLID | TileType.Flags.FLOOR );
-
-    registerTile( "resources/tiles/spr_tile_wall_top", Tiles.WALL_TOP );
-    registerTile( "resources/tiles/spr_tile_wall_top_left", Tiles.WALL_TOP_LEFT );
-    registerTile( "resources/tiles/spr_tile_wall_top_right", Tiles.WALL_TOP_RIGHT );
-    registerTile( "resources/tiles/spr_tile_wall_top_t", Tiles.WALL_TOP_T );
-    registerTile( "resources/tiles/spr_tile_wall_top_end", Tiles.WALL_TOP_END );
-
+    registerTile( "resources/tiles/spr_tile_wall_bottom_end", Tiles.WALL_BOTTOM_END );
     registerTile( "resources/tiles/spr_tile_wall_bottom_left", Tiles.WALL_BOTTOM_LEFT );
     registerTile( "resources/tiles/spr_tile_wall_bottom_right", Tiles.WALL_BOTTOM_RIGHT );
     registerTile( "resources/tiles/spr_tile_wall_bottom_t", Tiles.WALL_BOTTOM_T );
-    registerTile( "resources/tiles/spr_tile_wall_bottom_end", Tiles.WALL_BOTTOM_END );
-
-    registerTile( "resources/tiles/spr_tile_wall_side", Tiles.WALL_SIDE );
-    registerTile( "resources/tiles/spr_tile_wall_side_left_t", Tiles.WALL_SIDE_LEFT_T );
-    registerTile( "resources/tiles/spr_tile_wall_side_left_end", Tiles.WALL_SIDE_LEFT_END );
-    registerTile( "resources/tiles/spr_tile_wall_side_right_t", Tiles.WALL_SIDE_RIGHT_T );
-    registerTile( "resources/tiles/spr_tile_wall_side_right_end", Tiles.WALL_SIDE_RIGHT_END );
-
-    registerTile( "resources/tiles/spr_tile_wall_intersection", Tiles.WALL_INTERSECTION );
-    registerTile( "resources/tiles/spr_tile_wall_single", Tiles.WALL_SINGLE );
-
     registerTile( "resources/tiles/spr_tile_wall_entrance", Tiles.WALL_ENTRANCE );
-    registerTile( "resources/tiles/spr_tile_door_locked", Tiles.WALL_DOOR_LOCKED );
-    registerTile( "resources/tiles/spr_tile_door_unlocked", Tiles.WALL_DOOR_UNLOCKED, TileType.Flags.SOLID );
+    registerTile( "resources/tiles/spr_tile_wall_intersection", Tiles.WALL_INTERSECTION );
+    registerTile( "resources/tiles/spr_tile_wall_side", Tiles.WALL_SIDE );
+    registerTile( "resources/tiles/spr_tile_wall_side_left_end", Tiles.WALL_SIDE_LEFT_END );
+    registerTile( "resources/tiles/spr_tile_wall_side_left_t", Tiles.WALL_SIDE_LEFT_T );
+    registerTile( "resources/tiles/spr_tile_wall_side_right_end", Tiles.WALL_SIDE_RIGHT_END );
+    registerTile( "resources/tiles/spr_tile_wall_side_right_t", Tiles.WALL_SIDE_RIGHT_T );
+    registerTile( "resources/tiles/spr_tile_wall_single", Tiles.WALL_SINGLE );
+    registerTile( "resources/tiles/spr_tile_wall_top", Tiles.WALL_TOP );
+    registerTile( "resources/tiles/spr_tile_wall_top_end", Tiles.WALL_TOP_END );
+    registerTile( "resources/tiles/spr_tile_wall_top_left", Tiles.WALL_TOP_LEFT );
+    registerTile( "resources/tiles/spr_tile_wall_top_right", Tiles.WALL_TOP_RIGHT );
+    registerTile( "resources/tiles/spr_tile_wall_top_t", Tiles.WALL_TOP_T );
 
     _textureManager.startTextureLoad();
   }
