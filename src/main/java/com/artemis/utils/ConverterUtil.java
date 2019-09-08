74f54f07d18e948236a0a58ff1322381f3ac3dcb
Peter Donald <peter@realityforge.org>
2019-09-08 20:15:29 +1000

    Remove some unused methods

--- src/main/java/com/artemis/utils/ConverterUtil.java
+++ src/main/java/com/artemis/utils/ConverterUtil.java
@@ -1,6 +1,5 @@
 package com.artemis.utils;
 
-import java.util.BitSet;
 import javax.annotation.Nonnull;
 
 public final class ConverterUtil
@@ -16,42 +15,7 @@ public final class ConverterUtil
   }
 
   @Nonnull
-  public static IntBag toIntBag( @Nonnull final BitSet bs, @Nonnull final IntBag out )
-  {
-    if ( bs.isEmpty() )
-    {
-      out.setSize( 0 );
-      return out;
-    }
-
-    final int size = bs.cardinality();
-    out.setSize( size );
-    out.ensureCapacity( size );
-
-    final int[] activesArray = out.getData();
-    for ( int i = 0, id = -1, s = size; s > i; i++ )
-    {
-      id = bs.nextSetBit( id + 1 );
-      activesArray[ i ] = id;
-    }
-
-    return out;
-  }
-
-  @Nonnull
   public static BitVector toBitVector( @Nonnull final IntBag bag, @Nonnull final BitVector out )
-  {
-    final int[] data = bag.getData();
-    for ( int i = 0, s = bag.size(); s > i; i++ )
-    {
-      out.set( data[ i ] );
-    }
-
-    return out;
-  }
-
-  @Nonnull
-  public static BitSet toBitSet( @Nonnull final IntBag bag, @Nonnull final BitSet out )
   {
     final int[] data = bag.getData();
     for ( int i = 0, s = bag.size(); s > i; i++ )
